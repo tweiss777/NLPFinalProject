171fdcdbfb7e708616560ed74a1469f0a64b5e0c
Tal Weiss <Tweiss747@gmail.com>
2017-11-30 20:54:10 -0500

    web crawl testing in progress

--- WebParser.py
+++ WebParser.py
@@ -14,7 +14,8 @@ class WebParser():
     url = ""
 
     def __init__(self):
-        self.url = "https://www.nytimes.com/2017/11/30/theater/review-in-sleep-a-wakeful-woman-faces-long-surreal-nights.html"
+        #self.url = "https://www.nytimes.com/2017/11/30/theater/review-in-sleep-a-wakeful-woman-faces-long-surreal-nights.html"
+        self.url="https://www.nytimes.com/2017/11/30/us/politics/state-department-tillerson-pompeo-trump.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=first-column-region&region=top-news&WT.nav=top-news"
 
     ###
 
@@ -23,4 +24,7 @@ class WebParser():
         page = urllib.request.urlopen(self.url).read()
 
         soup = BeautifulSoup(page,'html.parser')
-        print(soup.prettify)
+        #print(soup.prettify)
+        article_body = soup.find_all('p','story-body-text') #will extract the text based on the hardcoded class name
+        for content in article_body:
+            print(content.getText())
