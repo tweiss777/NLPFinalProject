9b7323376c8b50e970bd1f72ca64e5e2761bda4d
Tal Weiss <tweiss747@gmail.com>
2017-12-20 01:13:20 -0500

    clustering handled

--- Clustering.py
+++ Clustering.py
@@ -7,8 +7,27 @@ class Clustering:
 
 
 
-    def tf_idf(self,doc_list):
+    def clusterize(self,doc_list):
+        k = int(input("enter number of clusters")) #input by the user of the number of clusters
         vectorizer = TfidfVectorizer(stop_words='english')
+        cluster_doc = {i: [] for i in range(k)} #key is the cluster number. value is an array of articles that are predicted to go to the cluster
+        vocab_cluster = {i: [] for i in range(k)}
         x = vectorizer.fit_transform(doc_list)
+        xa = x.toarray()
+        
+        model = KMeans(n_clusters=k,init='k-means++', max_iter=100, n_init=1)
+
+        centroids = model.cluster_centers_.argsort()[:,::-1]
+        vocab = vectorizer.get_feature_names() # gets the name of the terms from the integers
+        for i in range(k):
+            for ind in centroids:
+                vocab_cluster[i].append(vocab[ind0])
+`       
+        for doc in doc_list:
+            y = vectorizer.fit_transform([doc])
+            prediction = model.predict(y)
+            cluster_doc[prediction[0]].append(doc)
+
+        return vocab_cluster, cluster_doc
 
 
